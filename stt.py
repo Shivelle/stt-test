320de99783a3455bc8d11cbc08db3e81c779a94d
Shivelle <shivelle@gmx.net>
2022-05-14 16:42:27 +0200

    add comment for adjustable params

--- stt.py
+++ stt.py
@@ -7,5 +7,6 @@ sr_engine = sr.Recognizer()
 
 with sr.AudioFile(file_name) as file:
   data = sr_engine.record(file)
+  # change service / language depnding on your needs:
   text = sr_engine.recognize_google(data, language="de-DE")
   print(text)
